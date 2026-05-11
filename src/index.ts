4c7e6e8c4be94b95d8cbf110264ce28f14f17ebe
BotFragger <m.ronikusuma@gmail.com>
2026-05-11 19:13:08 +0700

    fix: database connection

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import { prisma } from "./utils/prisma";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("hello from express, after dockerfile changed");
+// app.get("/", async (req: Request, res: Response) => {
+//   res.send("hello from express");
+// });
+
+app.get("/", async (req: Request, res: Response) => {
+  const user = await prisma.user.findUnique({
+    where: { email: "budi@mail.com" },
+  });
+
+  if (!user) {
+    return res.send("user not found");
+  }
+
+  return res.json(user);
 });
 
 app.listen(port, () => {
